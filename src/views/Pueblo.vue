440dc07879f1832df755e3e79a014a148bcad080
Arturo Alfaro Rodriguez <arturo.alfaro87@gmail.com>
2021-01-17 16:18:28 -0300

    sin modal en pueblo

--- src/views/Pueblo.vue
+++ src/views/Pueblo.vue
@@ -1,20 +1,8 @@
 <template>
   <div class="container">
     <h1>
-      oro<span class="badge bg-warning ml-4 ">{{ oro }}</span>
+      Oro<span class="badge bg-warning ml-4 ">{{ oro }}</span>
     </h1>
-
-    <button id="show-modal" @click="showModal = true">Show Modal</button>
-    <!-- use the modal component, pass in the prop -->
-    <modal v-if="showModal" @close="showModal = false">
-      <!--
-      you can use custom content here to overwrite
-      default content
-    -->
-      <h3 slot="header">Mi header personalizado</h3>
-
-      <h3 slot="body">audio</h3>
-    </modal>
 
     <modal v-if="showModalCompositor" @close="showModalCompositor = false">
       <!--
@@ -39,23 +27,21 @@
     <div class="row my-5 mb-4">
       <div class="col">
         <img
-        src="../assets/suri.png"
-        class="img-fluid"
-        alt="suri"
+          src="../assets/suri.png"
+          class="img-fluid"
+          alt="suri"
           v-on:click="ganaOro(10)"
         />
       </div>
       <div class="col">
-      <button type="submit" @click="play">
-      <img
-          src="../assets/abuelo.png"
-          class="img-fluid"
-          alt="abuelo-aymara"
-          v-on:click="showModalCompositor = true"
-        />
-
-      </button>
-
+        <button type="submit" @click="play">
+          <img
+            src="../assets/abuelo.png"
+            class="img-fluid"
+            alt="abuelo-aymara"
+            v-on:click="showModalCompositor = true"
+          />
+        </button>
       </div>
       <div class="col">
         <img
@@ -75,30 +61,14 @@
         />
       </div>
       <div class="col">
-        <img
-          src="../assets/cactus.png"
-          class="img-fluid"
-          alt="cactus"
-        />
+        <img src="../assets/cactus.png" class="img-fluid" alt="cactus" />
       </div>
       <div class="col">
-        <img
-          src="../assets/vicuna.png"
-          class="img-fluid"
-          alt="vicuna"
-        />
+        <img src="../assets/vicuna.png" class="img-fluid" alt="vicuna" />
       </div>
     </div>
-    <div class="row">
-
-
-
-    </div>
-    <div class="row">
-
-
-
-    </div>
+    <div class="row"></div>
+    <div class="row"></div>
   </div>
 </template>
 
@@ -107,8 +77,8 @@ import { mapState } from "vuex";
 import Modal from "../components/Modal";
 import CompositorMusical from "../components/CompositorMusical";
 import TriviaAudioNpc from "../components/TriviaAudioNpc";
-import useSound from 'vue-use-sound';
-import buttonSfx from '../assets/musicaguitarra.mp3';
+import useSound from "vue-use-sound";
+import buttonSfx from "../assets/musicaguitarra.mp3";
 
 export default {
   data() {
@@ -130,18 +100,13 @@ export default {
     ...mapState(["oro"]),
   },
 
-  setup(){
-    const [play] = useSound(buttonSfx)
-     return{
-
+  setup() {
+    const [play] = useSound(buttonSfx);
+    return {
       play,
-    }
+    };
   },
-
-
 };
 </script>
 
-<style>
-
-</style>
+<style></style>
