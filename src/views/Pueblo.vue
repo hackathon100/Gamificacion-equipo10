0315a6ac24a483356f27c8dbbf8abe0085e5b65c
Arturo Alfaro Rodriguez <arturo.alfaro87@gmail.com>
2021-01-17 01:01:27 -0300

    se comienza a formar el pueblo

---
+++ src/views/Pueblo.vue
@@ -0,0 +1,110 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <button v-on:click="oro">
+          <img
+            src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+            class="img-fluid"
+            alt="..."
+          />
+        </button>
+      </div>
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+    </div>
+    <div class="row">
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+      <div class="col">
+        <img
+          src="https://www.turismodeobservacion.com/media/fotografias/norte-de-chile-cactus-y-flores-23684-xl.jpg"
+          class="img-fluid"
+          alt="..."
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  methods: {
+    oro: function() {
+      alert("ahora tengo mas oro");
+    },
+  },
+};
+</script>
+
+<style></style>
