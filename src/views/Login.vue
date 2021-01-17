f5fb8444f10685beec9709a427953120c0ee969b
Arturo Alfaro Rodriguez <arturo.alfaro87@gmail.com>
2021-01-17 16:45:26 -0300

    mini update login

--- src/views/Login.vue
+++ src/views/Login.vue
@@ -4,15 +4,12 @@
       <h1 class="">Bienvenido</h1>
 
       <b-row>
-        <b-col><img alt="Vue logo" src="../assets/logo.png" /></b-col>
+        <b-col><img alt="Vue logo" src="../assets/logo.png"/></b-col>
         <b-col class="my-5 text-center">
-          <b-form
-            @submit.prevent="loginUser"
-          >
-         
-
-            
-            <b-button type="submit" class=" boton" pill variant="success" lg="4" >Ingresar con tu cuenta GMAIL</b-button>
+          <b-form @submit.prevent="loginUser">
+            <b-button type="submit" class=" boton" pill variant="success" lg="4"
+              >Ingresar con tu cuenta GMAIL</b-button
+            >
             <a href="../analytics/analytics.html">Acceso Administrador</a>
           </b-form></b-col
         >
@@ -29,51 +26,40 @@ export default {
   name: "Login",
 
   data() {
-    return {
-      
-
-    
-    };
+    return {};
   },
   methods: {
     loginUser() {
-      
-        var provider = new firebase.auth.GoogleAuthProvider();
-        
-        firebase
-          .auth()
-          .signInWithPopup(provider)
-          .then( (result) =>{
-           
-            var user = result.user;
+      var provider = new firebase.auth.GoogleAuthProvider();
 
-            console.log(user);
-            console.log("login realizado");
-            // ...
-            this.$router.push({name: 'Login'});
-          })
-          
-          .catch((error) => {
-            // Handle Errors here.
-            console.log(error.code);
-            console.log(error.message);
-            console.log(error.email);
-            console.log(error.credential);
-          });
-    
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          var user = result.user;
+
+          console.log(user);
+          console.log("login realizado");
+          // ...
+          this.$router.push({ name: "Pueblo" });
+        })
+
+        .catch((error) => {
+          // Handle Errors here.
+          console.log(error.code);
+          console.log(error.message);
+          console.log(error.email);
+          console.log(error.credential);
+        });
     },
-    
   },
   components: {},
 
-  computed: {
-  
-  },
+  computed: {},
 };
 </script>
-<style >
- .boton{
-
+<style>
+.boton {
   text-align: center;
- }
+}
 </style>
